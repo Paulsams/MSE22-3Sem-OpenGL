f3f1751903668f59e1cefeb8e22270785a782bda
Paulsams <pasha.samsonov234@gmail.com>
2023-10-14 17:01:02 +0300

    Probably fixed Mac OS

--- src/Fractals/Sources/main.cpp
+++ src/Fractals/Sources/main.cpp
@@ -8,19 +8,24 @@ namespace
 constexpr auto g_sampels = 16;
 constexpr auto g_gl_major_version = 3;
 constexpr auto g_gl_minor_version = 3;
+constexpr auto g_depthBufferSize = 32;
 }// namespace
 
 int main(int argc, char ** argv)
 {
-	QApplication app(argc, argv);
+    QSurfaceFormat format;
+    format.setDepthBufferSize(g_depthBufferSize);
+    format.setSamples(g_sampels);
+    format.setVersion(g_gl_major_version, g_gl_minor_version);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
 
-	QSurfaceFormat format;
-	format.setSamples(g_sampels);
-	format.setVersion(g_gl_major_version, g_gl_minor_version);
-	format.setProfile(QSurfaceFormat::CoreProfile);
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+
+    QApplication app(argc, argv);
 
     Fractals::FractalView window;
-    window.setFormat(format);
     window.resize(640, 480);
 	window.show();
 
