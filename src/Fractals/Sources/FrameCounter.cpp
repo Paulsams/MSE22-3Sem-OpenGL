1d13c03ca26ef0f3a5772b9c2c8713bca678d1ee
Paulsams <pasha.samsonov234@gmail.com>
2023-10-21 18:39:52 +0300

    Change std::round on qRound

--- src/Fractals/Sources/FrameCounter.cpp
+++ src/Fractals/Sources/FrameCounter.cpp
@@ -1,8 +1,6 @@
 #include "FrameCounter.h"
 #include "DebugWindow.h"
 
-//#include <cmath>
-
 FrameCounter::FrameCounter(DebugWindow &debugWindow, size_t updateStepInMs)
         : updateStepInMs_(updateStepInMs), fpsCounter_(debugWindow.addReadonlyLabel("FPS", "FPS"))
     {
@@ -14,11 +12,11 @@ void FrameCounter::update() {
 
     if (static_cast<quint64>(timer_.elapsed()) >= updateStepInMs_)
     {
-//        const float elapsedSeconds = static_cast<float>(timer_.restart()) / static_cast<float>(updateStepInMs_);
-//        count_ = static_cast<size_t>(std::round(static_cast<float>(accumulativeCount_) / elapsedSeconds));
-//
-//        fpsCounter_->setText(std::to_string(count_).c_str());
-//        accumulativeCount_ = 0;
+        const float elapsedSeconds = static_cast<float>(timer_.restart()) / static_cast<float>(updateStepInMs_);
+        count_ = static_cast<size_t>(qRound(static_cast<float>(accumulativeCount_) / elapsedSeconds));
+
+        fpsCounter_->setText(std::to_string(count_).c_str());
+        accumulativeCount_ = 0;
     }
 }
 
